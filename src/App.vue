bea28f2799c2996dbe29f97cd689af54a4a908d6
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-02-23 14:55:05 +0100

    Début de l'affichage des UE

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+  import UE from "./components/UE.vue";
+  import { reactive } from "vue";
+  
+  let UEs = reactive([]);
+
+  UEs.push("Base de données")
+  UEs.push("Génie logiciel")
 </script>
 
 <template>
+  <UE
+    v-for="(UE) of UEs"
+    :intitule="UE"
+  />
   <h3>Hello world !!</h3>
 </template>
 
---
+++ src/components/UE.vue
@@ -0,0 +1,21 @@
+<script setup>
+  defineProps(["intitule"]);
+  defineEmits(["click"]);
+</script>
+
+<template>
+  <div>
+    {{ intitule }}
+  </div>
+</template>
+
+<style scoped>
+div {
+    display: inline-block; height: 100px; width: 150px;
+    border-color: rgb(3, 155, 229); border-style: solid;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; color: rgb(3, 155, 229);
+}
+div:hover {
+    border-color: rgb(124, 124, 124); background-color: rgb(124, 124, 124); color: rgb(255, 255, 255);
+}
+</style>
