d0263a0403d3da28088ff9e51ec1d11509897aa4
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-02-23 15:11:38 +0100

    CSS UE continué

--- src/components/UE.vue
+++ src/components/UE.vue
@@ -11,11 +11,12 @@
 
 <style scoped>
 div {
-    display: inline-block; height: 100px; width: 150px;
-    border-color: rgb(3, 155, 229); border-style: solid;
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; color: rgb(3, 155, 229);
+    display: inline-block; min-height: 130px; max-width: 300px; min-width: 220px;
+    border-color: rgb(3, 155, 229); border-style: solid; border-width: 10px;
+    font-family: "Roboto-Regular"; color: rgb(3, 155, 229); font-size: 14px;
+    text-align: center; margin-bottom: 20px; margin-left: 10px; margin-right: 10px; margin-top: 10px; margin-bottom: 20px;
 }
 div:hover {
-    border-color: rgb(124, 124, 124); background-color: rgb(124, 124, 124); color: rgb(255, 255, 255);
+    border-color: rgb(60, 60, 60); background-color: rgb(60, 60, 60); color: rgb(255, 255, 255);
 }
 </style>
