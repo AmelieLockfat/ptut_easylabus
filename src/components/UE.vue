de3ce4b34531f26bcce483375a2e0f49b41517d5
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-02-24 13:29:58 +0100

    Ajout de la police Roboto-Regular (Pour les cases (Pour mieux copier l'ENT))

--- src/components/UE.vue
+++ src/components/UE.vue
@@ -10,13 +10,20 @@
 </template>
 
 <style scoped>
+@font-face{
+  font-family: 'Roboto-Regular';
+  src: url('../assets/Roboto-Regular.ttf') format('truetype');
+  src: url('../assets/Roboto-Regular.woff') format('woff');
+  src: url('../assets/Roboto-Regular.woff2') format('woff2');
+  }
+
 div {
-    display: inline-block; min-height: 130px; max-width: 300px; min-width: 220px;
-    border-color: rgb(3, 155, 229); border-style: solid; border-width: 10px;
-    font-family: "Roboto-Regular"; color: rgb(3, 155, 229); font-size: 14px;
-    text-align: center; margin-bottom: 20px; margin-left: 10px; margin-right: 10px; margin-top: 10px; margin-bottom: 20px;
+  display: inline-block; min-height: 130px; max-width: 300px; min-width: 220px;
+  border-color: rgb(3, 155, 229); border-style: solid; border-width: 10px;
+  font-family: "Roboto-Regular"; color: rgb(3, 155, 229); font-size: 14px; text-transform:uppercase;
+  text-align: center; margin-bottom: 20px; margin-left: 10px; margin-right: 10px; margin-top: 10px; margin-bottom: 20px;
 }
 div:hover {
-    border-color: rgb(60, 60, 60); background-color: rgb(60, 60, 60); color: rgb(255, 255, 255);
+  border-color: rgb(60, 60, 60); background-color: rgb(60, 60, 60); color: rgb(255, 255, 255);
 }
 </style>
