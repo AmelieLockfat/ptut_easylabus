3c57ba476e4c259a8199f0c8c6f5f7c19eb0e28c
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-03-01 11:41:47 +0100

    debut de page d'identification

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,25 @@
   import { ref } from "vue";
   import Onglet_UE from "./components/Onglet_UE.vue";
   import Onglet_NIVEAU from "./components/Onglet_NIVEAU.vue";
+  import Onglet_IDENTIFICATION from "./components/Onglet_IDENTIFICATION.vue";
 
   let nivChoisie = ref(false)
   let intNiv = ref("")
+
+  let connecte = ref(false)
+  let identifiant = ref("")
+  let motDePasse = ref("")
+
+  function handlerSub (invite,ident,mdp){
+    if (invite){
+
+    }
+    else {
+      identifiant.value = ident;
+      motDePasse.value = mdp;
+    }
+    connecte.value = true;
+  }
 
   function handlerChoixNiv (intitule) {
     nivChoisie=true;
@@ -14,8 +30,9 @@
 
 <template>
   <p>{{ intNiv }}</p>
-  <Onglet_UE v-if="nivChoisie"/>
-  <Onglet_NIVEAU @choixNiv="handlerChoixNiv" v-else/>
+  <Onglet_UE v-if="nivChoisie&&connecte"/>
+  <Onglet_NIVEAU @choixNiv="handlerChoixNiv" v-else-if="connecte"/>
+  <Onglet_IDENTIFICATION @sub="handlerSub" v-else/>
 </template>
 
 <style>
---
+++ src/components/Onglet_IDENTIFICATION.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const emit = defineEmits(["sub"]);
+    const ident = ref("");
+    const mdp = ref("");
+
+    function emitterSub (invite){
+        if (invite) {
+            emit("sub",invite,null,null)
+        }
+        else {
+            emit("sub",invite,ident.value,mdp.value)
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="emitterSub(false)">
+            <input type="text" placeholder="identifiant:*" v-model="ident"/>
+            <input type="text" placeholder="mot de passe:*" v-model="mdp"/>
+            <input type="submit" value="Se connecter"/>
+            <input type="button" value="Accéder en tant qu'invité" @click="emitterSub(true)"/>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+</style>
