caaefa9f2f0f103a75ad61f7cf84467ca91c70f2
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-03-02 11:29:46 +0100

    Continuation du CSS de Onglet_IDENTIFICATION

--- src/components/Onglet_IDENTIFICATION.vue
+++ src/components/Onglet_IDENTIFICATION.vue
@@ -17,10 +17,13 @@
 
 <template>
     <div id="form">
-        <p>Entrez votre identifiant et votre mot de passe.</p>
+        <p>
+            <img src="src\assets\Bouclier.png" alt=""/>
+            Entrez votre identifiant et votre mot de passe.
+        </p>
         <form @submit.prevent="emitterSub(false)">
             <input id="ident" class="text" type="text" placeholder="identifiant:*" v-model="ident"/>
-            <input id="mdp" class="text" type="text" placeholder="mot de passe:*" v-model="mdp"/>
+            <input id="mdp" class="text" type="password" placeholder="mot de passe:*" v-model="mdp"/>
             <input class="sub" type="submit" value="Se connecter"/>
             <input class="sub" type="button" value="Accéder en tant qu'invité" @click="emitterSub(true)"/>
         </form>
@@ -33,14 +36,26 @@
         background-color: rgb(3, 155, 229); height: 450px; width: 700px; border-radius: 10px;
     }
 
+    img {
+        height: 25px; width: 21px;
+    }
+
+    p {
+        color: white; font-family: "Arial","Sans serif"; font-weight: bold; font-size: 20px;
+    }
+
     input.text {
         background-color: rgba(255, 255, 255, 0); width: 600px; height: 50px;
-        border-style: solid; border-color: white; border-radius: 10px;
+        border-style: solid; border-color: white; border-radius: 10px; border-width: 4px;
     }
 
     input.sub {
         text-transform: uppercase; border-style: none; border-radius: 5px; height: 40px; width: 80px;
         background-color: rgb(255, 129, 131); color: white; font-family: "Arial", "Sans serif";
+    }
+
+    input.sub:hover {
+        background-color: rgb(255, 100, 103);
     }
 
     div#lign {
