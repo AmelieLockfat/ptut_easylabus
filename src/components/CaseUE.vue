3e1c36e282d0b4f4c349c5f1a9bd8e4eb9a5e8f4
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-03-16 13:46:34 +0100

    fin du remplassement des cases (div ---> button)

--- src/components/CaseUE.vue
+++ src/components/CaseUE.vue
@@ -4,9 +4,9 @@
 </script>
 
 <template>
-  <div class="case" @click="$emit('clickc', intitule)">
-    <p>{{ intitule }}</p>
-  </div>
+  <button class="case" @click="$emit('clickc', intitule)">
+    {{ intitule }}
+  </button>
 </template>
 
 <style scoped>
@@ -17,24 +17,17 @@
     src: url('../assets/Roboto-Regular.woff2') format('woff2');
     }
 
-  div {
+  button {
     display: inline-block; height: 150px; width: 220px;
     border-style: solid; border-width: 10px;
     margin-bottom: 20px; margin-left: 10px; margin-right: 10px; margin-top: 10px; margin-bottom: 20px;
-    text-align: center; font-size: 18px; text-transform: uppercase;
+    font-size: 18px; text-transform: uppercase; font-weight: bold;
     font-family: "Roboto-Regular"; color: rgb(255, 255, 255);
     border-color: rgb(3, 155, 229);
 
   }
 
-  /*p {
-    height: 130px; width: 220px;
-    font-size:20px;
-    margin: auto; 
-        text-align: center;
-  }*//*Qu'on mette ça ou non il n'y a pas de changement*/
-
-  div:hover {
+  button:hover {
     background-color: rgb(3, 155, 229); color: rgb(40, 53, 147);
   }
 </style>
