bd027bb3faa48679463390688c67c37a9e3d64ba
AmelieLockfat1 <amelie.lock-fat@etud.univ-jfc.fr>
2023-04-15 12:54:46 +0200

    n

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +9,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  build: {
+    rollupOptions: {
+      external: ['src/assets/LogoISIS-Blanc.png','src/assets/GrosLogo.png','src/assets/CoeurISIS.png','src/assets/Bouclier.png','src/assets/logo.svg','src/assets/LogoISISFichEns.png']
+    },
+  },
+});
